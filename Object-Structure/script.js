1498f9fb1c5cb31f63a0f6f55afcbbf72781e85e
MAHSA <151853553+mhsaghsemii@users.noreply.github.com>
2024-07-18 22:20:46 +0430

    build(Data Type): Object structure model
    Added Object structure model in JavaScript

---
+++ Object-Structure/script.js
@@ -0,0 +1,131 @@
+// OBJECT-STRUCTURE
+let school = {
+    classA:{
+        std1:{
+            firstName:'Mahsa',
+            family:'Ghasemi',
+            phone:'09215892237',
+            address:'IRAN-Bushehr',
+            score:20
+        },
+        std2:{
+            firstName:'Zahra',
+            family:'Mohammadi',
+            phone:'09902586547',
+            address:'IRAN-Tehran',
+            score:12
+        },
+        std3:{
+            firstName:'Niki',
+            family:'Bagheri',
+            phone:'09459578855',
+            address:'IRAN-Bushehr',
+            score:20
+        },
+        std4:{
+            firstName:'Nazanin',
+            family:'Babaei',
+            phone:'09215892237',
+            address:'IRAN-Shiraz',
+            score:15
+        }
+    },
+    classB:{
+        std1:{
+            firstName:'Amir',
+            family:'Rad',
+            phone:'09365489922',
+            address:'IRAN-Bushehr',
+            score:20
+        },
+        std2:{
+            firstName:'Erfan',
+            family:'Mohammadi',
+            phone:'09945621236',
+            address:'IRAN-Tehran',
+            score:12
+        },
+        std3:{
+            firstName:'Ahoora',
+            family:'Bagheri',
+            phone:'09459578855',
+            address:'IRAN-Bushehr',
+            score:20
+        },
+        std4:{
+            firstName:'Ali',
+            family:'Babaei',
+            phone:'09215892237',
+            address:'IRAN-Shiraz',
+            score:15
+        }
+
+    }
+}
+// Student 1 - Class A
+console.log('Name: ',school.classA.std1.firstName)
+console.log('ّFamily: ',school.classA.std1.family)
+console.log('phone: ',school.classA.std1.phone)
+console.log('address: ',school.classA.std1.address)
+console.log('score: ',school.classA.std1.score)
+
+// Student 2 - Class A
+console.log('Name: ',school.classA.std2.firstName)
+console.log('ّFamily: ',school.classA.std2.family)
+console.log('phone: ',school.classA.std2.phone)
+console.log('address: ',school.classA.std2.address)
+console.log('score: ',school.classA.std2.score)
+
+// Student 3 - Class A
+console.log('Name: ',school.classA.std3.firstName)
+console.log('ّFamily: ',school.classA.std3.family)
+console.log('phone: ',school.classA.std3.phone)
+console.log('address: ',school.classA.std3.address)
+console.log('score: ',school.classA.std3.score)
+
+// Student 4 - Class A
+console.log('Name: ',school.classA.std4.firstName)
+console.log('ّFamily: ',school.classA.std4.family)
+console.log('phone: ',school.classA.std4.phone)
+console.log('address: ',school.classA.std4.address)
+console.log('score: ',school.classA.std4.score)
+
+// ------------
+
+// Student 1 - Class B
+console.log('Name: ',school.classB.std1.firstName)
+console.log('ّFamily: ',school.classB.std1.family)
+console.log('phone: ',school.classB.std1.phone)
+console.log('address: ',school.classB.std1.address)
+console.log('score: ',school.classB.std1.score)
+
+// Student 2 - Class B
+console.log('Name: ',school.classB.std2.firstName)
+console.log('ّFamily: ',school.classB.std2.family)
+console.log('phone: ',school.classB.std2.phone)
+console.log('address: ',school.classB.std2.address)
+console.log('score: ',school.classB.std2.score)
+
+// Student 3 - Class B
+console.log('Name: ',school.classB.std3.firstName)
+console.log('ّFamily: ',school.classB.std3.family)
+console.log('phone: ',school.classB.std3.phone)
+console.log('address: ',school.classB.std3.address)
+console.log('score: ',school.classB.std3.score)
+
+// Student 4 - Class B
+console.log('Name: ',school.classB.std4.firstName)
+console.log('ّFamily: ',school.classB.std4.family)
+console.log('phone: ',school.classB.std4.phone)
+console.log('address: ',school.classB.std4.address)
+console.log('score: ',school.classB.std4.score)
+
+
+
+
+
+
+
+
+
+
