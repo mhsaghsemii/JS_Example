d3d2855b9eb6a0eb2780d7890ad23bace4b1749e
MAHSA <151853553+mhsaghsemii@users.noreply.github.com>
2024-08-04 09:30:04 +0430

    build(Search Condition): Definition Condition for Search
    Definition Variable Condition for Compare two Variable

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,29 +1,29 @@
-// let Difination for Calculator
+// let Definition for Calculator
 
 let calc = 0,num1= 16, num2 = 4
 
-// Sum Operation of two Varible
+// Sum Operation of two Variable
 calc = num1 + num2
 // Show Sum
 console.log(`${num1} + ${num2}` = calc);
 
 // ------------------------------
 
-// Minus Operation of two Varible
+// Minus Operation of two Variable
 calc = num1 - num2
 // Show Minus
 console.log(`${num1} - ${num2}` = calc);
 
 // ------------------------------
 
-// Multiply Operation of two Varible
+// Multiply Operation of two Variable
 calc = num1 - num2
 // Show Multiply
 console.log(`${num1} * ${num2}` = calc);
 
 // ------------------------------
 
-// Division Operation of two Varible
+// Division Operation of two Variable
 calc = num1 - num2
-// Show Multiply
+// Show Division
 console.log(`${num1} / ${num2}` = calc);
---
+++ Search/script.js
@@ -0,0 +1,15 @@
+// Variable Definition for Compare
+let string = 'Hello World!', target = 'World'
+
+// Show the string Variable
+console.log('String: ',string)
+console.log('Target: ',target)
+
+// Compare two Variable 'string' and 'target'
+if(string.includes(target)){
+    // the Output if Condition is True
+    console.log(`the ${string} contains the ${target}`)
+}else{
+        // the Output if Condition is False
+    console.log(`the ${string} doesn't contains the ${target}`)
+}
