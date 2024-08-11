a8b02cc47474a6e03db54f8e18d40d1b510bd6bd
MAHSA <151853553+mhsaghsemii@users.noreply.github.com>
2024-08-11 11:15:30 +0430

    build(Return Characters): Definition Function For Extracts Length Characters From the String
    Added var for output, Function for Extracts Length Characters From the String AND return the Output .

---
+++ Func-3/script.js
@@ -0,0 +1,16 @@
+// let Definition for Output the Result
+let output
+
+// Function for Return a String Containing the Specified Part of the Given String 
+function count(string, numberOfCharecter){
+
+    // Extracts Length Characters From the String
+    output = string.substr("",numberOfCharecter)
+
+    // Show Characters
+    // Counting From the Start Index.
+    return output
+}
+
+// Print Result
+console.log(count("Hello World", 3));
